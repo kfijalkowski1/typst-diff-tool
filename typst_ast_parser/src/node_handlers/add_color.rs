63cec1ed2e815713137122cbd134dd23dc5b722d
gambolkf <gambolkf@gmail.com>
2024-06-03 18:45:11 +0200

    feat: color only one line not whole paragraph

--- typst_ast_parser/src/node_handlers/add_color.rs
+++ typst_ast_parser/src/node_handlers/add_color.rs
@@ -1,4 +1,5 @@
 use typst_syntax::{SyntaxKind, SyntaxNode};
+use similar::{ChangeTag, TextDiff};
 
 use crate::enums::custom_enums::NodeStatus;
 use crate::node_handlers::helpers_with_syntax_kind::{is_some_kind_of_call, is_some_kind_of_whitespace};
@@ -33,12 +34,48 @@ pub fn add_color_to_every_block(node: &SyntaxNode, node_status: NodeStatus, prev
 }
 
 pub fn create_combined_text_diff(child_old: &SyntaxNode, child_new: &SyntaxNode, is_an_argument_value: bool) -> String {
-    let mut combined_text: String = format!("#text(fill: red)[{}]#text(fill: green)[{}]", child_old.text(), child_new.text());
+    let old_text = child_old.text().to_string();
+    let new_text = child_new.text().to_string();
 
-    if is_an_argument_value {
+
+    let diff = TextDiff::from_lines(
+        &old_text,
+        &new_text,
+    );
+
+    let mut node_res: String = "".to_string();
+
+    for change in diff.iter_all_changes() {
+        let new_text = match change.tag() {
+            ChangeTag::Delete => {
+                if let Some(child_str) = change.as_str() {
+                    color_text(child_str, &is_an_argument_value, "red".to_string())
+                } else {
+                    "".to_string()
+                }
+            },
+            ChangeTag::Insert => {
+                if let Some(child_str) = change.as_str() {
+                    color_text(child_str, &is_an_argument_value, "green".to_string())
+                } else {
+                    "".to_string()
+                }
+            },
+
+            ChangeTag::Equal  => child_new.text().to_string()
+        };
+        node_res.push_str(&new_text);
+    }
+    node_res
+}
+
+
+fn color_text(child: &str, is_an_argument_value: &bool, color: String) -> String {
+    let mut combined_text = format!("#text(fill: {})[{}]", color, child);
+
+    if *is_an_argument_value {
         combined_text = combined_text.replace("\"", "");
         combined_text = format!("[{}]", combined_text);
     }
-
     combined_text
 }
